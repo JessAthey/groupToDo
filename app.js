5f45f077dc8ae41c25c4268147be9071e9dc51bb
JessAthey <jelaineya@gmail.com>
2023-03-29 20:08:24 -0400

    refined input functions to handle empty input. Added a delete button. Updated styling.

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
-let toDoList = [];
+const todoList = [];
 
+function addToDoItem(item) {
+  const todoItemExists = todoList.some(
+    (existingItem) => existingItem.title === item.title
+  );
 
-function addToDoItem(item){
-    toDoList.push(item);
+  if (todoItemExists) {
+    alert("This todo item already exists.");
+  } else {
+    item.dateAdded = new Date();
+    todoList.push(item);
     renderTodoList();
+  }
 }
-
 
 let todoInput = document.getElementById("todo-input");
 
 let addTodoButton = document.querySelector("button[type='submit']");
 
-
 addTodoButton.addEventListener("click", function (event) {
   event.preventDefault();
 
+  if(todoInput.value.trim() === ""){
+    alert("1st Todo: Enter a valid input");
+    return;
+  }
+
   let newTodo = {
     title: todoInput.value,
-
     isCompleted: false,
-
     isStarted: false,
-
     dateAdded: new Date(),
-
-    ItemId: toDoList.length,
+    ItemId: todoList.length,
   };
 
   addToDoItem(newTodo);
@@ -32,21 +39,33 @@ addTodoButton.addEventListener("click", function (event) {
 });
 
 function renderTodoList() {
-    // Render the todo list items in the HTML here
-    let todoList = document.getElementById("todo-list");
-    todoList.innerHTML = ""; // Clear the existing list items
-  
-    for (let i = 0; i < toDoList.length; i++) {
-       todoItem = toDoList[i];
-       listItem = document.createElement("li");
-       title = document.createElement("span");
-      let status = document.createElement("span");
-  
-      title.textContent = todoItem.title;
-      status.textContent = todoItem.isCompleted ? "Completed" : "Incomplete";
-  
-      listItem.appendChild(title);
-      listItem.appendChild(status);
-      todoList.appendChild(listItem);
-    }
+  const todoListElement = document.getElementById("todo-list");
+  todoListElement.innerHTML = "";
+
+  for (const todoItem of todoList) {
+    const listItem = document.createElement("li");
+    const title = document.createElement("span");
+    const status = document.createElement("span");
+    const deleteButton = document.createElement("button");
+    const dateAdded = document.createElement("span");
+
+    title.textContent = todoItem.title;
+    status.textContent = todoItem.isCompleted ? "Completed" : "Incomplete";
+    deleteButton.textContent = "Finish Task";
+    deleteButton.classList.add("deleteBtn");
+    dateAdded.textContent = "Added on: " + todoItem.dateAdded.toLocaleString();
+
+    deleteButton.addEventListener("click", () => {
+      const index = todoList.indexOf(todoItem);
+      todoList.splice(index, 1);
+      renderTodoList();
+    });
+
+    listItem.appendChild(deleteButton);
+    listItem.appendChild(title);
+    listItem.appendChild(document.createElement("br"));
+    listItem.appendChild(status);
+    listItem.appendChild(dateAdded);
+    todoListElement.appendChild(listItem);
   }
+}
